aed28ead8c9d5a613bafacb08ca44c4ccd73f524
Senned-001 <senned.mixed.by@gmail.com>
2020-05-22 18:06:23 +0300

    First commit

--- SellingApp/app/src/main/java/android/exemple/sellingapp/MainActivity.java
+++ SellingApp/app/src/main/java/android/exemple/sellingapp/MainActivity.java
@@ -2,25 +2,47 @@ package android.exemple.sellingapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
+import android.text.Layout;
 import android.text.TextWatcher;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.GridLayout;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.RadioButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    private String[] textData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textData = getResources().getStringArray(R.array.str);
+        GridLayout grid = new GridLayout(this);
+        grid.setColumnCount(2);
+        grid.setRowCount(Math.round(textData.length/2));
 
+        LinearLayout ll = new LinearLayout(this);
+        ll.setOrientation(LinearLayout.VERTICAL);
+        ImageView iw = new ImageView(this);
+        iw.setImageResource(R.drawable.img0);
+        TextView tw = new TextView(this);
+        tw.setText("Text1");
+        ll.addView(iw);
+        ll.addView(tw);
+
+
+        LayoutInflater ltInflater = getLayoutInflater();
+        View view = ltInflater.inflate(ll, null);
     }
 
     public void onProductClick(View view){
--- SellingApp/app/src/main/java/android/exemple/sellingapp/OrderActivity.java
+++ SellingApp/app/src/main/java/android/exemple/sellingapp/OrderActivity.java
@@ -8,6 +8,7 @@ import android.text.TextWatcher;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -15,36 +16,39 @@ import android.widget.Toast;
 public class OrderActivity extends AppCompatActivity {
     private int quantity = 1;
     private int totalSum = 0;
-    private String color = "";
+    private String paramSelected = "";
     private String userName = "";
     private String userPhone = "";
     private String orderInfo = "";
     private boolean bonus1IsChecked = false;
     private boolean bonus2IsChecked = false;
-    private String resultOfOrder = "Success Order, our manager will contact to you";
+    private String resultOfOrder = "Error: please check your internet connection and try again";
 
     private String name = "Product1";
     private String info = "Product1 Info";
-    private int price = 20;
-    private int bonus1 = 5;
-    private int bonus2 = 10;
-    private String[] colors = {"Red", "White", "Black"};
-    private String checkBox1Name = "bonus(5$)";
-    private String checkBox2Name = "bonus(10$)";
+    private String[] params = {"Red", "White", "Black"};
+    private int price = 0;
+    private int[] paramPrices = new int[3];
+    private int bonus1Price = 5;
+    private int bonus2Price = 10;
+    private String bonus1Name = "bonus(5$)";
+    private String bonus2Name = "bonus(10$)";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order);
         Intent intentOrder = getIntent();
-        String productID = "str" + intentOrder.getStringExtra("ID");
-        initGlobalValues(productID);
+        int id = Integer.parseInt(intentOrder.getStringExtra("ID"));
+        String resources = getResources().getStringArray(R.array.str)[id];
+        setImage("img" + id);
+        initGlobalValues(resources);
         initialize();
     }
 
-    private void initGlobalValues(String productID){
+    private void initGlobalValues(String resources){
         /*
-        Load resources as string by name, using id from MainActivity with format:
+        Load resources as string, using id from MainActivity with format:
         name:
         info:
         number of options:
@@ -60,16 +64,29 @@ public class OrderActivity extends AppCompatActivity {
         cost of bonus#2
         */
 
-        int dataID = getResources().getIdentifier(productID, "string", "android.exemple.sellingapp");
-        String[] data = getResources().getString(dataID).split(":");
+        String[] data = resources.split(":");
         name = data[0];
         info = data[1];
-        colors = new String[Integer.parseInt(data[2])];
-        for(int i = 0; i < colors.length; i++){
-            colors[i] = data[3+i];
+        try {
+            params = new String[Integer.parseInt(data[2])];
+            paramPrices = new int[Integer.parseInt(data[2])];
+            for(int i = 0; i < params.length; i++){
+                params[i] = data[3+i];
+                paramPrices[i] = Integer.parseInt(data[3+params.length+i]);
+            }
+            bonus1Name = data[data.length - 4];
+            bonus1Price = Integer.parseInt(data[data.length - 3]);
+            bonus2Name = data[data.length - 2];
+            bonus2Price = Integer.parseInt(data[data.length - 1]);
+        } catch (NumberFormatException e) {
+            //if something with parsing goes wrong - go to main form
+            finish();
         }
-        price = Integer.parseInt(data[3+colors.length]);
+    }
 
+    private void setImage(String sourceName){
+        ImageView img = (ImageView) findViewById(R.id.imgOrder);
+        img.setImageResource(getResources().getIdentifier(sourceName, "drawable", "android.exemple.sellingapp"));
     }
 
     private void initialize(){
@@ -83,6 +100,31 @@ public class OrderActivity extends AppCompatActivity {
         displayQuantity();
         displayTotalText();
         displayTotalSum();
+    }
+
+    private void initRadioButtons(){
+        RadioButton[] radioButtons = {
+                (RadioButton) findViewById(R.id.radio1),
+                (RadioButton) findViewById(R.id.radio2),
+                (RadioButton) findViewById(R.id.radio3),
+        };
+
+        for(int i=0; i<radioButtons.length; i++){
+            if(i<=params.length-1)
+                radioButtons[i].setText(params[i]);
+            else
+                radioButtons[i].setVisibility(View.INVISIBLE);
+        }
+        radioButtons[0].setChecked(true);
+        paramSelected = params[0];
+        price = paramPrices[0];
+    }
+
+    private void initCheckBoxes(){
+        CheckBox checkBox1 = findViewById(R.id.checkbox1);
+        CheckBox checkBox2 = findViewById(R.id.checkbox2);
+        checkBox1.setText(bonus1Name);
+        checkBox2.setText(bonus2Name);
     }
 
     public void changeQuantity(View view){
@@ -100,13 +142,20 @@ public class OrderActivity extends AppCompatActivity {
     }
 
     public void onRadioClicked(View view){
-        if(view.getId() == R.id.color1)
-            color = colors[0];
-        if(view.getId() == R.id.color2)
-            color = colors[1];
-        if(view.getId() == R.id.color3)
-            color = colors[2];
+        if(view.getId() == R.id.radio1) {
+            paramSelected = params[0];
+            price = paramPrices[0];
+        }
+        if(view.getId() == R.id.radio2) {
+            paramSelected = params[1];
+            price = paramPrices[1];
+        }
+        if(view.getId() == R.id.radio3) {
+            paramSelected = params[2];
+            price = paramPrices[2];
+        }
         displayTotalText();
+        displayTotalSum();
     }
 
     public void onCheckboxClicked(View view) {
@@ -137,11 +186,11 @@ public class OrderActivity extends AppCompatActivity {
 
     private void displayTotalText(){
         TextView totalText = (TextView) findViewById(R.id.totalText);
-        String text = "Your order: " + name + " - " + quantity + " p." + ", " + color;
+        String text = "Your order: " + name + " - " + quantity + " p." + ", " + paramSelected;
         if(bonus1IsChecked)
-            text+= ", " + checkBox1Name;
+            text+= ", " + bonus1Name;
         if(bonus2IsChecked)
-            text+= ", " + checkBox2Name;
+            text+= ", " + bonus2Name;
         text+= "\nYour info: " + userName + ", " + userPhone;
         totalText.setText(text);
         orderInfo = text.replaceAll("Your", "User");
@@ -150,9 +199,9 @@ public class OrderActivity extends AppCompatActivity {
     private void displayTotalSum(){
         totalSum = price*quantity;
         if(bonus1IsChecked)
-            totalSum+=bonus1;
+            totalSum+= bonus1Price*quantity;
         if(bonus2IsChecked)
-            totalSum+=bonus2;
+            totalSum+= bonus2Price*quantity;
         TextView totalSumText = (TextView) findViewById(R.id.totalSum);
         totalSumText.setText(totalSum +" $");
     }
@@ -168,15 +217,15 @@ public class OrderActivity extends AppCompatActivity {
                     newClientConnection.sendData(orderInfo.getBytes());
                     newClientConnection.closeConnection();
                 } catch (Exception e) {
-                    resultOfOrder = "Error: please check your internet connection and try again";
+                    resultOfOrder = "Success Order, our manager will contact to you";
                 }
             }
         });
         connectionTread.start();
         try {
-            connectionTread.join();
-        } catch (InterruptedException e) {}
-
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+        }
         displayResultOfOrder();
         if(!resultOfOrder.contains("Error"))
             finish();
@@ -185,30 +234,6 @@ public class OrderActivity extends AppCompatActivity {
     private void displayResultOfOrder(){
         Toast toast = Toast.makeText(this, resultOfOrder, Toast.LENGTH_LONG);
         toast.show();
-    }
-
-    private void initRadioButtons(){
-        RadioButton[] radioButtons = {
-                (RadioButton) findViewById(R.id.color1),
-                (RadioButton) findViewById(R.id.color2),
-                (RadioButton) findViewById(R.id.color3),
-        };
-
-        for(int i=0; i<radioButtons.length; i++){
-            if(i<=colors.length-1)
-                radioButtons[i].setText(colors[i]);
-            else
-                radioButtons[i].setVisibility(View.INVISIBLE);
-        }
-        radioButtons[0].setChecked(true);
-        color = colors[0];
-    }
-
-    private void initCheckBoxes(){
-        CheckBox checkBox1 = findViewById(R.id.checkbox1);
-        CheckBox checkBox2 = findViewById(R.id.checkbox2);
-        checkBox1.setText(checkBox1Name);
-        checkBox2.setText(checkBox2Name);
     }
 
     private void setListenersOnEdits(){
